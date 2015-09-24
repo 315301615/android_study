cb8baec4d84918512a149e25e4d4518b5fe0acc1
zhang shuai <zs198893@gmail.com>
2015-09-25 03:47:08 +0800

    第一次

---
+++ acticonbar/src/main/java/com/example/zhangshuai/android_study/acticonbar/ShowMessageActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhangshuai.android_study.acticonbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_message);
+    }
+}
