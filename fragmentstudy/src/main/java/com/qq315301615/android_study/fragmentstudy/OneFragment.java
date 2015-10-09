db3669e1c705f9386e45be37a37f8f89cf8c8291
zhang shuai <zs198893@gmail.com>
2015-10-09 18:26:52 +0800

    1
    1

--- fragmentstudy/src/main/java/com/qq315301615/android_study/fragmentstudy/OneFragment.java
+++ fragmentstudy/src/main/java/com/qq315301615/android_study/fragmentstudy/OneFragment.java
@@ -1,6 +1,7 @@
 package com.qq315301615.android_study.fragmentstudy;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -27,8 +28,8 @@ public class OneFragment extends Fragment {
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
     @Override
