b203c96f2ff1cb6d453b07a7a019c33386fd4d20
zhang shuai <zs198893@gmail.com>
2016-03-09 01:12:44 +0800

    正在学习data binding

---
+++ mvp1/src/main/java/com/example/mvp1/bean/User.java
@@ -0,0 +1,15 @@
+package com.example.mvp1.bean;
+
+/**
+ * Created by zhanglin on 16/3/9.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
