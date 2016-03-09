f64150c2c3f62d6261c83f00a1d57ae1719c4dae
zhang shuai <zs198893@gmail.com>
2016-03-09 17:34:42 +0800

    data binding
    只实现了单向绑定

--- mvp1/src/main/java/com/example/mvp1/MainActivity.java
+++ mvp1/src/main/java/com/example/mvp1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.mvp1;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.tool.DataBindingBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.mvp1.bean.User;
+import com.example.mvp1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("zhang","shuai");
+        mainBinding.setUser(user);
     }
 }
--- mvp1/src/main/java/com/example/mvp1/bean/User.java
+++ mvp1/src/main/java/com/example/mvp1/bean/User.java
@@ -4,12 +4,27 @@ package com.example.mvp1.bean;
  * Created by zhanglin on 16/3/9.
  */
 public class User {
-    public final String firstName;
-    public final String lastName;
+    public String firstName;
+    public String lastName;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
