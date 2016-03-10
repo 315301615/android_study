40eb5b66cbcd7ee9e6c0a1ce2e2a2fb28f35121b
zhang shuai <zs198893@gmail.com>
2016-03-10 17:44:51 +0800

    吴

---
+++ mvp1/src/main/java/com/example/mvp1/module/login/presenter/ILoginPresenter.java
@@ -0,0 +1,7 @@
+package com.example.mvp1.module.login.presenter;
+
+/**
+ * Created by 123 on 2016/3/10.
+ */
+public class ILoginPresenter {
+}
---
+++ mvp1/src/main/java/com/example/mvp1/module/login/view/ILoginView.java
@@ -0,0 +1,7 @@
+package com.example.mvp1.module.login.view;
+
+/**
+ * Created by 123 on 2016/3/10.
+ */
+public class ILoginView {
+}
